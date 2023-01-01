ef64f1e91aeed9d6b2445ca07aa786a031ad2d35
shanzefenglai <yuria.ay@gmail.com>
2023-01-01 23:40:12 +0800

    ✨ 新增首页

---
+++ recommend/src/main/java/com/recommend/controller/ListController.java
@@ -0,0 +1,13 @@
+package com.recommend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class ListController {
+    @RequestMapping("/list")
+    public String list() {
+        return "list";
+    }
+}
